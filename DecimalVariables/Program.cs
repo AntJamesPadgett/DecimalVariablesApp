d6b7ae301ecf4d91ac2858251b39899a6d86c08b
antjamespadgett <padgettanthonyj@gmail.com>
2022-01-26 11:32:36 -0600

    Completed exercise

---
+++ DecimalVariables/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+//4.12,32,1.234
+// 4.12, 32, 1.234
+
+//int32 - 00000000 00000000 00000000 00000001  all used to store one number, allot of memory allocated
+// decimal more space needed vs double..doubles way to go... decimals used for $$ and large units.
+
+
+decimal bankAccountBalance;
+bankAccountBalance = 2.34M;
+
+Console.WriteLine(bankAccountBalance);
+
+
+
